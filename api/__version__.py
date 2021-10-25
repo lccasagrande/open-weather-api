f3a00306a6c7c59007bb103db3231d4197455876
Lucas Casagrande <lcamelocasagrande@gmail.com>
2021-10-25 14:13:15 -0300

    Add project template files

---
+++ api/__version__.py
@@ -0,0 +1,8 @@
+"""Metadata for the OpenWeather API."""
+
+__title__ = "open_weather_api"
+__description__ = "The Open Weather API"
+__version__ = "0.0.1"
+__author__ = "Lucas Casagrande"
+__author_email__ = "lcamelocasagrande@gmail.com"
+__license__ = "MIT"
